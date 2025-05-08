0d689b7a8649e51832850acba500bc963c8108e8
April Hall <ari@arithefirst.com>
2025-05-08 12:52:14 -0400

    feat: ShadCN & Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <></>;
+  return <div className="min-h-screen w-screen">
+    <header className="w-screen p-2 border-b border-b-border">
+      <span className="font-bold mr-1">{process.env.NEXT_PUBILC_COMPANYNAME}</span>
+      Inventory Manager
+    </header>
+  </div>;
 }
