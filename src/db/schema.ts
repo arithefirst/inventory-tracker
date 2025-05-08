52b07371c276399dc594aae8246d41ad3fc64498
April Hall <ari@arithefirst.com>
2025-05-08 13:11:18 -0400

    ci: Prettier

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,10 @@
-import { integer, text, pgTable, timestamp, jsonb } from "drizzle-orm/pg-core";
+import { integer, text, pgTable, timestamp, jsonb } from 'drizzle-orm/pg-core';
 
-export const items = pgTable("item", {
-  id: integer("id").primaryKey(),
-  name: text("name").notNull(),
+export const items = pgTable('item', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
   createdAt: timestamp('created_at', { mode: 'date' }).notNull(),
-  image: text("image").notNull(),
+  image: text('image').notNull(),
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  customData: jsonb("custom_data").$type<Record<string, any> | null>().default(null)
+  customData: jsonb('custom_data').$type<Record<string, any> | null>().default(null),
 });
