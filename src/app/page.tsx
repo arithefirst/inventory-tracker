f6576edbc12df6cbc74408860d915d29f8e26ff9
April Hall <ari@arithefirst.com>
2025-05-08 13:10:35 -0400

    feat: Dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { ThemeToggle } from "@/components/themeToggle";
+
 export default function Home() {
   return <div className="min-h-screen w-screen">
-    <header className="w-screen p-2 border-b border-b-border">
+    <header className="w-screen p-2 border-b border-b-border flex items-center">
+      <p>
       <span className="font-bold mr-1">{process.env.NEXT_PUBILC_COMPANYNAME}</span>
       Inventory Manager
+      </p>
+      <ThemeToggle className="ml-auto"/>
     </header>
   </div>;
 }
